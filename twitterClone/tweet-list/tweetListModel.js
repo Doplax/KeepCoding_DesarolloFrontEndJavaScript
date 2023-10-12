4431f4a1112850a082628184ea9732f94142f115
Doplax <doplax@gmail.com>
2023-10-12 19:43:42 +0200

    Implementación de try catch en el modelo

--- twitterClone/tweet-list/tweetListModel.js
+++ twitterClone/tweet-list/tweetListModel.js
@@ -11,8 +11,21 @@ const transformTweets = (tweets) => {
 
 export const getTweets = async () => {
     const url = "https://feke-tweets-api-kccerce.app/posts"
+    let parsedTweets = [];
+    // Async Await
 
-    // Promesas
+    try {
+        const response = await fetch(url)
+        const tweets = await response.json()
+        parsedTweets = transformTweets(tweets)
+
+    } catch (error) {
+        console.log('Algo malo ocurrió');
+    }
+
+    return parsedTweets
+
+    //Promesas
     //return new Promise((resolve, reject) => {
     //    fetch(url)
     //        .then((response) => response.json())
@@ -21,10 +34,4 @@ export const getTweets = async () => {
     //            resolve(parsedTweets);
     //        })
     //});
-
-    // Async Await
-    const response = await fetch(url)
-    const tweets = await response.json()
-    const parsedTweets = transformTweets(tweets)
-    return parsedTweets
 }
