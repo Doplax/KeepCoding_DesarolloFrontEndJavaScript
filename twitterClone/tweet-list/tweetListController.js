4ba4db1cfa3a37de47d6fcaf79fc2753dcd6fb25
Doplax <doplax@gmail.com>
2023-10-16 16:40:12 +0200

    Gestion con window.alert() para cuando no hay tweets

--- twitterClone/tweet-list/tweetListController.js
+++ twitterClone/tweet-list/tweetListController.js
@@ -2,14 +2,21 @@ import { getTweets } from "./tweetListModel";
 //import
 
 
-export const tweetListController = (tweetList) => {
-    getTweets().then(tweets =>{
+export const tweetListController = async (tweetList) => {
+    const tweets = await getTweets()
+
+    if (tweets.length === 0) {
+        window.alert("No hay tweets disponibles")
+    } else {
         tweets.forEach(tweet => {
             const tweetContainer = document.createElement('div');
             tweetContainer.classList.add('tweetContainer');
-
+    
             tweetContainer.innerHTML = buildTweet(tweet)
             tweetList.appendChild(tweetContainer)
         });
-    } )
+    }
+
+ 
+    
 }
