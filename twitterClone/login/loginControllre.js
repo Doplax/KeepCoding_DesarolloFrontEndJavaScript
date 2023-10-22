201e2cebede14717e02675e1c476a0fa827305d5
Doplax <doplax@gmail.com>
2023-10-22 16:28:53 +0200

    Creación de la logica de lógin

--- twitterClone/login/loginControllre.js
+++ twitterClone/login/loginControllre.js
@@ -1,3 +1,34 @@
-export const loginController = () => {
+import { loginUser } from "./loginModel"
+
+export const loginController = async (loginForm) => {
+
+    loginForm.addEventListener('submit', () => submitLogin(loginForm))
     
+
+    // Gestionar respuesta
+}
+
+const submitLogin = async (loginForm) => {
+
+    const { email , password } = getLoadingData(loginForm)
+
+        // Login contra sparrest
+        try {
+            await loginUser(email, password)
+            alert('Login Ok')
+        } catch (error) {
+            alert(error)
+        }
+}
+
+const getLoadingData = (loginForm) => {
+    //Obtener datos del formulario
+    const formData = new FormData(loginForm)
+    const email = formData.get('email')
+    const password = formData.get('password')
+
+    return  {
+        email,
+        password
+    }
 }
