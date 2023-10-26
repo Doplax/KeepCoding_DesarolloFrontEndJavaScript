abb928b01dcfa4e78e47ec9fdf1c93874d5193ad
Doplax <doplax@gmail.com>
2023-10-26 20:52:36 +0200

    commit en carpeta superior

--- twitterClone/tweet-detail/tweetDetailController.js
+++ twitterClone/tweet-detail/tweetDetailController.js
@@ -1,11 +1,14 @@
-import { getTweet } from './tweetDetailModel.js'
+import { getTweet ,deleteTweet } from './tweetDetailModel.js'
+import { buildTweet } from './tweetDetailView.js';
 import { dispatchEvent } from './dispatchEvent.js'
+import { decodeToken } from '../utils/decodeToken.js';
 
 export const tweetDetailController = async (tweetDetail, tweetId) => {
     
     try {
         const tweet = await getTweet(tweetId);
         tweetDetail.innerHTML = buildTweet(tweet)
+        handleDeleteTweet(tweet, tweetDetail);
     } catch (error) {
         dispatchEvent('tweetLoaded', {type: "error", message: "El tweet no existe", tweetDetail})
         setTimeout(() => {
@@ -15,4 +18,29 @@ export const tweetDetailController = async (tweetDetail, tweetId) => {
 
     }
 
+}
+
+const handleDeleteTweet = (tweet, tweetDetail) => {
+    
+    const token = window.localStorage.getItem('token');
+
+    if (token) {
+        const { userId } = decodeToken(token);
+        if (userId === tweet.userId) {
+            addDeteleTweetButton(tweet, tweetDetail) 
+        }
+    }
+}
+
+const addDeteleTweetButton = (tweet, tweetDetail) => {
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Borrar tweet';
+    deleteButton.addEventListener('click',async () =>{
+        if (confirm('Seguro que quieres borrar este tweet?'))
+        await deleteTweet(tweet.id)
+
+        window.location = 'index.html'
+    })
+
+    tweetDetail.appendChild(deleteButton);
 }
--- twitterClone/tweet-detail/tweetDetailModel.js
+++ twitterClone/tweet-detail/tweetDetailModel.js
@@ -1,12 +1,12 @@
 const parseTweet = (tweet) => {
     return {
         handler: tweet.author.toUpperCase(),
-        photo: tweet.image,
-        likes: tweet.likes,
-        message: tweet.message
+        message: tweet.message,
+        likes: [],
+        userId: tweet.user.id,
+        id: user.id
     }
 }
-
 
 export const getTweet = async (tweetId) => {
     const url = `https://localhost:8000/api/tweets/${tweetId}`
@@ -22,4 +22,21 @@ export const getTweet = async (tweetId) => {
         throw error.message;
     }
     return parseTweet(tweet)
+}
+
+export const deleteTweet = async (tweetId) => {
+    const url = `https://localhost:8000/api/tweets/${tweetId}`
+    let tweet;
+    try {
+        const response = await fetch(url)
+        if (response.ok) {
+            const tweet = response.json();
+        } else {
+            throw new Error('El Tweet no existe ')
+        }
+    } catch (error) {
+        throw error.message;
+    }
+    return parseTweet(tweet)
+
 }
